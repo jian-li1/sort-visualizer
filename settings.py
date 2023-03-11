e0a2db1a0b5a89123d4ae417c3c76dd4a9636a3a
Jian Li <86862325+Jian-Li1@users.noreply.github.com>
2023-03-10 22:22:42 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -56,13 +56,28 @@ PIXEL_CHANGE = [i / 100 for i in range(25, (RECT_WIDTH + RECT_SPACING + 1) * 100
 #[i for i in range(1, RECT_WIDTH + RECT_SPACING + 1) if (RECT_WIDTH + RECT_SPACING) % i == 0]
 
 # Initialize variables for keeping track of the program's status
-sorting = threading.Event() # Is set when sorting operation is active
-playing = threading.Event() # Is set when sorting operation is being played
-swapping = threading.Event() # Is set when swapping is being animated
-btn_animate = threading.Event() # Is set when user hovers over a button; animates color change of the button
+status = {
+    'sorting': False, # Is set when sorting operation is active
+    'playing': threading.Event(), # Is set when sorting operation is being played
+    'swapping': False, # Is set when swapping is being animated
+    'moving': False, # Is set when rectangle is moving up or down
+    'sub_sorting': False, # Is set when sorting sub arrays during merge sort
+    'merging': False, # Is set when merging is being animated
+    'btn_animate': False # Is set when user hovers over a button; animates color change of the button
+}
+
+# Speed adjust and delay of sorting
+class Speed():
+    adjust = len(PIXEL_CHANGE) // 3
+    delay = (RECT_WIDTH + RECT_SPACING) / PIXEL_CHANGE[adjust] / FPS['idle'] / 60 * FPS['idle']
 
 # Pivot line for quick sorting
 quick_sort_line = {'start': None, 'end': None}
+# Infomation for swapping two rectangles: both rectangles' index in the array and their positions (x coordinates)
+swap_rect = {'rect1': None, 'rect1_pos': None, 'rect2': None, 'rect2_pos': None}
+# Information for moving two rectangles for merge sorting
+sub_sort = {'rect': None, 'rect_x_pos': None, 'rect_pos_dest': None}
+merge_rect = [None, None]
 
 # Selected algorithm
-selected = {'alg': None, 'function': None}
+selected = {'alg': None, 'text': '', 'function': None}
