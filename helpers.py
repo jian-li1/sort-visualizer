5e4dd76acaa0911efa35ed3c84b0dd6593f6d89b
Jian Li <86862325+Jian-Li1@users.noreply.github.com>
2023-03-10 22:22:19 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -17,7 +17,8 @@ class Rectangle():
 
     def draw(self) -> None:
         # Display the rectangle and its outline
-        if not sorting.is_set(): self.color = WHITE
+        if not status['sorting']: 
+            self.color = WHITE
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
         pygame.draw.rect(screen, self.border, (self.x, self.y, self.width, self.height), 3)
         # Display the number below each rectangle
@@ -39,6 +40,7 @@ class Button():
         self.font = pygame.font.SysFont(None, font)
         self.active = True
         self.clicked = False
+        self.visible = True
     
     def draw(self) -> None:
         # Display the button and its outline
@@ -84,36 +86,94 @@ def draw_buttons(buttons: list) -> None:
         # Animate color change of the button when cursor hovers over it
         # Also animates color change when button is inactivated
         if btn.rect.collidepoint(pygame.mouse.get_pos()) and btn.color > HOVER and not btn.clicked:
-            btn_animate.set()
+            status['btn_animate'] = True
             btn.color = (btn.color[0] - min(rgb_change, btn.color[0] - HOVER[0]),
                          btn.color[1] - min(rgb_change, btn.color[1] - HOVER[1]),
                          btn.color[2] - min(rgb_change, btn.color[2] - HOVER[2]))
         # Revert back to original color of the button when cursor no longer hovers over it
         elif not btn.rect.collidepoint(pygame.mouse.get_pos()) and btn.color < WHITE and btn.active and not btn.clicked:
-            btn_animate.set()
+            status['btn_animate'] = True
             btn.color = (btn.color[0] + min(rgb_change, WHITE[0] - btn.color[0]),
                          btn.color[1] + min(rgb_change, WHITE[1] - btn.color[1]),
                          btn.color[2] + min(rgb_change, WHITE[2] - btn.color[2]))
         btn.draw()
 
-def swap_rectangles(rectangles: list, swap_rect: dict, speed_adjust: list) -> None:
+def swap_rectangles(rectangles: list) -> None:
     # Swapping is complete if two rectangles have exchanged their positions
     if rectangles[swap_rect['rect1']].x == swap_rect['rect2_pos'] and rectangles[swap_rect['rect2']].x == swap_rect['rect1_pos']:
-        swapping.clear()
+        status['swapping'] = False
         return
 
-    # Calculate the pixel difference between the two rectangles and scale up or down the swapping speed accordingly
+    # Calculate the pixel difference between the two rectangles and scale the swapping speed accordingly
     # This can ensure that the swapping animation of the two farthest rectangles is as fast as the swapping animation of the two closest rectangles
     distance = swap_rect['rect2_pos'] - swap_rect['rect1_pos']
     scaling = int(distance / (RECT_WIDTH + RECT_SPACING))
     
     # Scale the pixel per frame according to the current FPS
-    pixel_per_frame = PIXEL_CHANGE[speed_adjust[0]] * scaling * (60 / FPS['active'])
+    pixel_per_frame = PIXEL_CHANGE[Speed.adjust] * scaling * (60 / FPS['active'])
 
     # Change both rectangles' x coordinates until one approaches the other's position
     if rectangles[swap_rect['rect1']].x < swap_rect['rect2_pos']:
-        # If pixel difference between destination and rectangle 1 is less than the pixels per frame, change pixel by the difference instead
+        # If pixel difference between stopping position and rectangle 1 position is less than the pixels per frame, change pixel by the difference instead
         rectangles[swap_rect['rect1']].x += min(pixel_per_frame, swap_rect['rect2_pos'] - rectangles[swap_rect['rect1']].x)
     if rectangles[swap_rect['rect2']].x > swap_rect['rect1_pos']:
-        # If pixel difference between rectangle 2 and destination is less than the pixels per frame, change pixel by the difference instead
+        # If pixel difference between rectangle 2 position and stopping position is less than the pixels per frame, change pixel by the difference instead
         rectangles[swap_rect['rect2']].x -= min(pixel_per_frame, rectangles[swap_rect['rect2']].x - swap_rect['rect1_pos'])
+
+def sorting_sub_rectangles(rectangles: list) -> None:
+    # Subarray sorting is complete if rectangle have approached its position 
+    if rectangles[sub_sort['rect']].x == sub_sort['rect_pos_dest'] and int(rectangles[sub_sort['rect']].y + rectangles[sub_sort['rect']].height) == 500:
+        status['sub_sorting'] = False
+        return
+    
+    # Calculate the x-coordinate pixel difference between the rectangle's current position and stopping position and scale the moving speed accordingly
+    x_distance = abs(sub_sort['rect_pos_dest'] - sub_sort['rect_x_pos'])
+    x_scaling = int(x_distance / (RECT_WIDTH + RECT_SPACING))
+
+    # Scale the x-coordinate pixel per frame according to the current FPS
+    x_pixel_per_frame = PIXEL_CHANGE[Speed.adjust] * x_scaling * (60 / FPS['active']) / 2
+    
+    # Scale the y-coordinate pixel per frame according to the x-coordinate pixel per frame
+    if x_distance != 0:
+        y_pixel_per_frame = x_pixel_per_frame * 250 / x_distance
+    else:
+        y_pixel_per_frame = PIXEL_CHANGE[Speed.adjust] * int(250 / (RECT_WIDTH + RECT_SPACING)) * (60 / FPS['active']) / 2
+
+    # Change each rectangle's x and y coordinates
+    # If pixel difference between current rectangle and the stopping position is less than the pixel per frame, change pixel by the difference instead
+    if rectangles[sub_sort['rect']].x < sub_sort['rect_pos_dest']:
+        rectangles[sub_sort['rect']].x += min(x_pixel_per_frame, sub_sort['rect_pos_dest'] - rectangles[sub_sort['rect']].x)
+    elif rectangles[sub_sort['rect']].x > sub_sort['rect_pos_dest']:
+        rectangles[sub_sort['rect']].x -= min(x_pixel_per_frame, rectangles[sub_sort['rect']].x - sub_sort['rect_pos_dest'])
+
+    rectangles[sub_sort['rect']].y += min(y_pixel_per_frame, 500 - (rectangles[sub_sort['rect']].y + rectangles[sub_sort['rect']].height))
+
+def merge_rectangles(rectangles: list) -> None:
+    # Merging is complete if all rectangles have moved back up to their original positions
+    if rectangles[merge_rect[0]].y + rectangles[merge_rect[0]].height == 250:
+        status['merging'] = False
+        return
+    
+    # Scale the moving speed accordingly and the pixel per frame according to the current FPS
+    y_scaling = int(250 / (RECT_WIDTH + RECT_SPACING))
+    y_pixel_per_frame = PIXEL_CHANGE[Speed.adjust] * y_scaling * (60 / FPS['active']) / 2
+
+    for rect in range(merge_rect[0], merge_rect[1]):
+        # If pixel difference between current rectangle and the stopping position is less than the pixel per frame, change pixel by the difference instead
+        rectangles[rect].y -= min(y_pixel_per_frame, rectangles[rect].y + rectangles[rect].height - 250)
+
+def move_rectangles_up(rectangles: list) -> None:
+    # Move base of all rectangles up to y = 250px
+    if rectangles[0].y + rectangles[0].height > 250:
+        for rectangle in rectangles:
+            rectangle.y -= 3
+    elif rectangles[0].y + rectangles[0].height == 250:
+        status['moving'] = False
+
+def move_rectangles_down(rectangles: list) -> None:
+    # Move all rectangles back down to original base
+    if rectangles[0].y + rectangles[0].height < BASE:
+        for rectangle in rectangles:
+            rectangle.y += 3
+    elif rectangles[0].y + rectangles[0].height == BASE:
+        status['moving'] = False
